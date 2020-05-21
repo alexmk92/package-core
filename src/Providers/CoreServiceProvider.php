267f2105c70076a0eeb61549ef703c3f3fec2b22
Alex Sims <alexander.sims92@gmail.com>
2020-05-21 08:21:01 +0100

    Base commit

---
+++ src/Providers/CoreServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AlexMK92\StockService\Providers;
+
+use AlexMK92\StockService\Http\StockServiceController;
+use Illuminate\Support\ServiceProvider;
+
+class CoreServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
